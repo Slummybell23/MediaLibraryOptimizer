c07dbd133b15f2d368042b9e25127e16f602e8a5
slummybell <cachesalyers@gmail.com>
2025-03-24 13:44:48 -0500

    Set quality to 20...

--- LibraryOptimizer/VideoInfo.cs
+++ LibraryOptimizer/VideoInfo.cs
@@ -119,7 +119,7 @@ public class VideoInfo
             _reEncodeHevcProfile8Command = $"ffmpeg -i '{_profile8HevcFile}' -c:v hevc_nvenc -preset p7 -cq 3 -c:a copy '{_encodedHevc}'";
         else
             //INTEL ARC Hopefully encodes well now
-            _reEncodeHevcProfile8Command = $"ffmpeg -hwaccel qsv -i '{_profile8HevcFile}' -c:v hevc_qsv -preset 1 -global_quality 12 -c:a copy '{_encodedHevc}'";
+            _reEncodeHevcProfile8Command = $"ffmpeg -hwaccel qsv -i '{_profile8HevcFile}' -c:v hevc_qsv -preset 1 -global_quality 20 -c:a copy '{_encodedHevc}'";
         
         //Generate Temp Folder
         CreateTempFolder();
